4268e3f5c5ef9191c541466f73da2aa2ef01d1e2
Christian Manley-Peterman <christianpeterman@gmail.comgit config --global user.name CM-P-fl0wgit config --gobal user.email christianpeterman@gmail.com>
2025-02-08 15:06:24 -0600

    Added basic tests

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from app import app  
+
+@pytest.fixture
+def client():
+    """Create a test client for Flask app"""
+    app.config["TESTING"] = True 
+    with app.test_client() as client:
+        yield client  
+
+def test_home(client):
+    """Test if home route is working"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome" in response.data  
